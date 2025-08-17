fa7920b03e2c1e46b8ad5514a007c3494177c66b
arpande97 <architpande888@gmail.com>
2025-08-17 19:02:00 -0400

    CF D1714 java

---
+++ src/CF/D1714.java
@@ -0,0 +1,605 @@
+// package CF;
+
+import java.util.*;
+import java.io.*;
+
+public class D1714 {
+    static int LOG = 30;
+    static Reader sc = new Reader();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st = new StringTokenizer("");
+    static final int IMAX = Integer.MAX_VALUE, IMIN = Integer.MIN_VALUE, IMOD = (int)1e9 + 7;
+    static final long LMAX = Long.MAX_VALUE, LMIN = Long.MIN_VALUE, LMOD = (long)1e9 + 7;
+
+
+    public static void main(String[] args) throws Exception {
+        int t = ni();
+        while (t-- > 0) {
+            solve();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    private static void solve() throws IOException {
+        String t = ns();
+        int n = ni();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = ns();
+        }
+        int m = t.length();
+        int[] dp = new int[m + 1];
+        int[] used = new int[m + 1];
+        int[] next = new int[m + 1];
+        for (int i = m - 1; i >= 0; i--) {
+            int matched = 0, idx = -1;
+            for (int j = 0; j < s.length; j++) {
+                boolean ok = true;
+                String str = s[j];
+                int k = 0;
+                for (int p = i; p < m && k < str.length(); p++, k++) {
+                    if (t.charAt(p) != str.charAt(k)) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok && k == str.length()) {
+                    if (k >= matched) {
+                        matched = k;
+                        idx = j;
+                    }
+                }
+            }
+            if (matched == 0) {
+                dp[i] = 1000;
+                used[i] = idx;
+            }
+            else {
+                int best = 1000;
+                int id = -1;
+                for (int inc = 1; inc <= matched; inc++) {
+                    if (dp[i + inc] < best) {
+                        best = dp[i + inc];
+                        id = inc;
+                    }
+                }
+                dp[i] = 1 + best;
+                next[i] = id;
+                used[i] = idx;
+            }
+        }
+        if (dp[0] >= 1000) {
+            println(-1);
+            return;
+        }
+        println(dp[0]);
+        int ptr = 0;
+        while (ptr < m) {
+            printSp(used[ptr] + 1);
+            printSp(ptr + 1);
+            ptr += next[ptr];
+            bw.newLine();
+        }
+    }
+    
+
+
+
+
+    private static int find(int x, int[] parent) {
+        if (x == parent[x])
+            return x;
+        return find(parent[x], parent);
+    }
+    private static void union(int x, int y, int[] parent, int[] rank) {
+        //run parent[i] = find(parent[i], parent) for all at the end if you want the leaders
+        int X = find(x, parent), Y = find(y, parent);
+        if (X == Y)
+            return;
+        if (rank[X] < rank[Y])
+            parent[X] = Y;
+        else if (rank[X] > rank[Y])
+            parent[Y] = X;
+        else {
+            parent[Y] = X;
+            rank[X]++;
+        }
+    }
+    //send this function a sorted array to generate the all the permutations
+    private static boolean nextPermutation(int[] arr) {
+        int idx = -1, n = arr.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                idx = i;
+                break;
+            }
+        }
+        if (idx == -1)
+            return false;
+        int j = -1;
+        for (int i = n - 1; i > idx; i--) {
+            if (arr[i] > arr[idx]) {
+                j = i;
+                break;
+            }
+        }
+        int tmp = arr[idx];
+        arr[idx] = arr[j];
+        arr[j] = tmp;
+        int l = idx + 1, r = n - 1;
+        while (l < r) {
+            tmp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = tmp;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    static class SegmentTree {
+        int[] tree, lazy;
+        int n;
+
+        public SegmentTree(int[] arr) {
+            n = arr.length;
+            tree = new int[4 * n];
+            lazy = new int[4 * n];
+            build(arr, 1, 0, n - 1);
+        }
+
+        private void build(int[] arr, int node, int l, int r) {
+            if (l == r) {
+                tree[node] = arr[l];
+                return;
+            }
+            int mid = (l + r) / 2;
+            build(arr, 2 * node, l, mid);
+            build(arr, 2 * node + 1, mid + 1, r);
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
+
+        private void push(int node, int l, int r) {
+            if (lazy[node] != 0) {
+                tree[node] += (r - l + 1) * lazy[node]; // Apply lazy value
+                if (l != r) { // Propagate to children
+                    lazy[2 * node] += lazy[node];
+                    lazy[2 * node + 1] += lazy[node];
+                }
+                lazy[node] = 0; // Clear lazy value at current node
+            }
+        }
+
+        public void update(int ql, int qr, int val) {
+            update(1, 0, n - 1, ql, qr, val);
+        }
+
+        private void update(int node, int l, int r, int ql, int qr, int val) {
+            push(node, l, r);
+            if (r < ql || l > qr) return; // No overlap
+            if (ql <= l && r <= qr) {
+                lazy[node] += val;
+                push(node, l, r);
+                return;
+            }
+            int mid = (l + r) / 2;
+            update(2 * node, l, mid, ql, qr, val);
+            update(2 * node + 1, mid + 1, r, ql, qr, val);
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
+        }
+
+        public int query(int ql, int qr) {
+            return query(1, 0, n - 1, ql, qr);
+        }
+
+        private int query(int node, int l, int r, int ql, int qr) {
+            push(node, l, r);
+            if (r < ql || l > qr) return 0; // No overlap
+            if (ql <= l && r <= qr) return tree[node]; // Total overlap
+            int mid = (l + r) / 2;
+            return query(2 * node, l, mid, ql, qr) + query(2 * node + 1, mid + 1, r, ql, qr);
+        }
+    }
+
+      /*
+     * ------------------Black boxes--------------------
+     */
+
+    //Mo's
+    static class Query {
+        int l, r, idx;
+        Query(int l, int r, int idx) {
+            this.l = l;
+            this.r = r;
+            this.idx = idx;
+        }
+    }
+    static class Mo {
+        int currL = 0, currR = -1;
+        int[] arr;
+        int n;
+        int blockSize;
+        Mo(int[] arr) {
+            this.arr = arr;
+            this.n = arr.length;
+            this.blockSize = (int)Math.sqrt(n);
+        }
+        public void sort(List<Query> queries) {
+            Collections.sort(queries, (a, b) -> {
+                int aBlock = a.l/blockSize;
+                int bBlock = b.l/blockSize;
+                if (aBlock != bBlock)
+                    return aBlock - bBlock;
+                return a.r - b.r;
+            });
+        }
+        public void process(List<Query> queries) {
+            for (Query query : queries) {
+                int left = query.l, right = query.r, idx = query.idx;
+                while (currL > left) {
+                    currL--;
+                    add(currL);
+                }
+                while (currR < right) {
+                    currR++;
+                    add(currR);
+                }
+                while (currL < left) {
+                    remove(currL);
+                    currL++;
+                }
+                while (currR > right) {
+                    remove(currR);
+                    currR--;
+                }
+            }
+        }
+        public void add(int idx) {
+
+        }
+        public void remove(int idx) {
+
+        }
+    }
+    //Sqrt decomposition
+
+    static class Sqrt {
+        static int blockSize;
+        static long[] blocks;
+        static int[] nums;
+        Sqrt(int sqrtn, int[] arr) {
+            blockSize = sqrtn;
+            blocks = new long[sqrtn];
+            nums = arr;
+            for (int i = 0; i < nums.length; i++) {
+                blocks[i/blockSize] += nums[i];
+            }
+        }
+        static void update(int x, int v) {
+            blocks[x/blockSize] -= nums[x];
+            nums[x] = v;
+            blocks[x/blockSize] += nums[x];
+        }
+        static long query(int l, int r) {
+            return query(r) - query(l - 1);
+        }
+        static long query(int r) {
+            if (r == -1)
+                return 0;
+            long res = 0;
+            for (int i = 0; i < r/blockSize; i++) {
+                res += blocks[i];
+            }
+            for (int i = (r/blockSize) * blockSize; i <= r; i++) {
+                res += nums[i];
+            }
+            return res;
+        }
+    }
+//---------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    static class Pii {
+        int a, b;
+        Pii(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Pii)) return false;
+            Pii other = (Pii) o;
+            return this.a == other.a && this.b == other.b;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b);
+        }
+    }
+    static class Pll {
+        long a, b;
+        Pll(long a, long b) {
+            this.a = a;
+            this.b = b;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Pll)) return false;
+            Pll other = (Pll) o;
+            return this.a == other.a && this.b == other.b;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b);
+        }
+
+    }
+    static class Pil {
+        int a;
+        long b;
+        Pil(int a, long b) {
+            this.a = a;
+            this.b = b;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Pil)) return false;
+            Pil other = (Pil) o;
+            return this.a == other.a && this.b == other.b;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b);
+        }
+
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                baos.write(c);
+            }
+            return baos.toString("UTF-8");
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    public static int ni()throws IOException
+    {
+        return sc.nextInt();
+    }
+    public static long nl()throws IOException
+    {
+        return sc.nextLong();
+    }
+    public static double nd()throws IOException
+    {
+        return sc.nextDouble();
+    }
+    public static String ns()throws IOException
+    {
+        String str = sc.readLine().trim();
+        if (str.isEmpty())
+            str = sc.readLine().trim();
+        return str;
+    }
+    public static char nc() throws IOException {
+        byte c;
+        do {
+            c = sc.read();
+        } while (c <= ' ');
+        return (char) c;
+    }
+    public static String nt() throws IOException {
+        while (!st.hasMoreTokens()) {
+            String line = sc.readLine();
+            if (line == null) return null;
+            st = new StringTokenizer(line);
+        }
+        return st.nextToken();
+    }
+    public static void print(String a)throws IOException
+    {
+        bw.write(a);
+    }
+    public static void printSp(String a)throws IOException
+    {
+        bw.write(a+" ");
+    }
+    public static void println(String a)throws IOException
+    {
+        bw.write(a+"\n");
+    }
+    public static void print(int a) throws IOException {
+        bw.write(Integer.toString(a));
+    }
+
+    public static void println(int a) throws IOException {
+        bw.write(Integer.toString(a));
+        bw.newLine();
+    }
+
+    public static void print(long a) throws IOException {
+        bw.write(Long.toString(a));
+    }
+
+    public static void println(long a) throws IOException {
+        bw.write(Long.toString(a));
+        bw.newLine();
+    }
+
+    public static void printSp(int a) throws IOException {
+        bw.write(Integer.toString(a));
+        bw.write(' ');
+    }
+
+    public static void printSp(long a) throws IOException {
+        bw.write(Long.toString(a));
+        bw.write(' ');
+    }
+
+
+}
